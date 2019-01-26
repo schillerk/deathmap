5fbbc68705722a69730f27b84b76d6013a9fa309
Kyle Schiller <kyle@Kyles-MacBook-Pro.local>
2019-01-26 03:40:48 -0500

    remove paragraphs

--- client/src/App.js
+++ client/src/App.js
@@ -102,60 +102,19 @@ class App extends Component {
       margin: 'auto',
     };
 
+    const people = this.state.deaths == 1 ? 'person has' : 'people have';
+
     return (
       <div className="App">
         <div className="title-wrap">
           <div className="title">
-            A map of every human death in real time
-          </div>
-          <div className="subtitle">
-            (more or less)
+            Human Deaths in Real Time
+            <div className="subtitle">
+              Since you opened this page, {this.state.deaths} {people} died.
+            </div>
           </div>
         </div>
         <div id='map' style={mapStyle}></div>
-        <p className="caption">
-          Since you opened this page, {this.state.deaths} people have died.
-        </p>
-        <p className="top-caption">
-          Here are the 10 leading causes:
-        </p>
-        <Pie />
-        <p className="subheader">
-          Secondary Effects, Hidden Killers
-        </p>
-        <p>
-          It’s worth noting that many of these causes are merely the secondary effects of broader structural problems.
-        </p>
-        <p>
-          For example, air pollution isn’t a direct cause of death, but is still responsible for 7 million deaths each year. According to data from the WHO, many of the top killers are air pollution in disguise, with  24% of deaths from stoke (1.4M / year), 25% of deaths from heart disease (2.4M / year) and 43% of deaths from lung disease (1.8M / year) attributable to air pollution.
-        </p>
-        <p className="subheader">
-          Data-Driven Questioning
-        </p>
-        <p>
-          Death is a pretty bad proxy for suffering, so none of this should be translated uncritically into career, ethical or global priority recommendations. Still, mapping human mortality this way helps provoke questions that can help guide thinking in those domains.
-        </p>
-        <p>
-          What makes a death acceptable or premature?
-        </p>
-        <p>
-          Which causes of death imply a life of suffering for the deceased or for their loved ones?
-        </p>
-        <p>
-          Who’s deaths are worth preventing?
-        </p>
-        <p className="subheader">
-          Effective Giving
-        </p>
-        <p>
-          Some deaths are difficult and costly to prevent, others, not so much.
-        </p>
-        <p>
-          Give Well’s 2018 Cost-Effectiveness Analysis estimates that donating to the most effective charities in the world can result in an outcome equivalent to averting the death of an individual under 5 for as little as $858.
-        </p>
-        <p>
-          Though diseases like Malaria (.44M deaths / year) and Schistosomiasis (between .024M and .2M deaths / year) aren’t responsible for enough absolute deaths to show up on our previous chart, they’re among the cheapest to prevent, and have, according to Give Well estimates, the highest marginal benefit per donated dollar of any disease.
-        </p>
       </div>
     );
   }
